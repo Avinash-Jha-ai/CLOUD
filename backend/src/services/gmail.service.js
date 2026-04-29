338cb96c1554f4e820bc91ca48cfcc89dff16c26
Avinash Jha <Avinash-Jha-ai@users.noreply.github.com>
2026-04-29 13:22:49 +0530

    hi

--- backend/src/services/gmail.service.js
+++ backend/src/services/gmail.service.js
@@ -2,21 +2,16 @@ import nodemailer from "nodemailer";
 import { config } from "../configs/config.js";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: config.EMAIL_USER,
     pass: config.EMAIL_PASS,
   },
-  connectionTimeout: 10000,
-  socketTimeout: 10000,
-  family: 4, // Force IPv4
 });
 
 export const sendEmail = async (to, subject, html) => {
   await transporter.sendMail({
-    from: `Avinash jha <${config.EMAIL_USER}>`, 
+    from: `"Cloudavi" <${config.EMAIL_USER}>`, 
     to,
     subject,
     html, 
